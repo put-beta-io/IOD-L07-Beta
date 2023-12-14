e771dcc150162dca6504b63a53a8417f40115606
jagtym <jagtymxd@gmail.com>
2023-12-14 22:29:11 +0100

    Add TextTransformerBuilder class

---
+++ src/test/java/pl/put/poznan/transformer/logic/UppercaseDecoratorTest.java
@@ -0,0 +1,38 @@
+package pl.put.poznan.transformer.logic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UppercaseDecoratorTest {
+    private TextTransformer transformer;
+
+    @BeforeEach
+    void setUp() {
+        TextTransformerBuilder builder = new TextTransformerBuilder();
+        builder.addTransformation("upper");
+        transformer = builder.getTransformer();
+    }
+
+    @Test
+    void testDecorateUppercaseAllLower() {
+        String got = transformer.transform("cheetah");
+
+        assertEquals("CHEETAH", got);
+    }
+
+    @Test
+    void testDecorateUppercaseMixed() {
+        String got = transformer.transform("aBcDeF");
+
+        assertEquals("ABCDEF", got);
+    }
+
+    @Test
+    void testDecorateUppercaseWithSpaces() {
+        String got = transformer.transform("a b c d e f g");
+
+        assertEquals("A B C D E F G", got);
+    }
+}
