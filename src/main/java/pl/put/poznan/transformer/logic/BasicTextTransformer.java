c0daf7bcbc4fd902eda98c7a62c0b96555930154
jagtym <jagtymxd@gmail.com>
2023-12-14 22:27:06 +0100

    Add BasicTextTransformer implementing TextTransformer

---
+++ src/main/java/pl/put/poznan/transformer/logic/BasicTextTransformer.java
@@ -0,0 +1,9 @@
+package pl.put.poznan.transformer.logic;
+
+public class BasicTextTransformer implements TextTransformer {
+    @Override
+    public String transform(String text) {
+        return text;
+    }
+}
+
