c6eb2db6997a3246ad12ec1344c6b9087d43f0a1
jagtym <jagtymxd@gmail.com>
2023-12-14 22:28:50 +0100

    Add CapitallizeDecorator

---
+++ src/main/java/pl/put/poznan/transformer/logic/CapitalizeDecorator.java
@@ -0,0 +1,29 @@
+package pl.put.poznan.transformer.logic;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CapitalizeDecorator extends BasicTextTransformer {
+    private final TextTransformer transformer;
+
+    public CapitalizeDecorator(TextTransformer wrappedTransformer) {
+        transformer = wrappedTransformer;
+    }
+
+    @Override
+    public String transform(String text) {
+        return capitalize(transformer.transform(text));
+    }
+
+    private String capitalize(String text) {
+        var split = List.of(text.split(" "));
+        var words = new ArrayList<String>();
+        for (String word : split) {
+            String capitalize = word.substring(0, 1).toUpperCase() + word.substring(1);
+            words.add(capitalize);
+        }
+        return String.join(" ", words);
+    }
+}
