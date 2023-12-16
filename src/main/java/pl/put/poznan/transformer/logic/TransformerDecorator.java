d22c839be2cdebb392f78165963005a0b30fd4de
jagtym <jagtymxd@gmail.com>
2023-12-16 16:22:23 +0100

    Add javadoc documentation

--- src/main/java/pl/put/poznan/transformer/logic/TransformerDecorator.java
+++ src/main/java/pl/put/poznan/transformer/logic/TransformerDecorator.java
@@ -1,12 +1,25 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * Abstract clas for text transformers.
+ * Each transformer should implement this interface.
+ */
 public class TransformerDecorator implements TextTransformer {
     private final TextTransformer transformer;
 
+    /**
+     * Constructor for TransformerDecorator
+     * @param wrappedTransformer Transformer to be wrapped
+     */
     public TransformerDecorator(TextTransformer wrappedTransformer) {
         transformer = wrappedTransformer;
     }
 
+    /**
+     * Method for transforming text
+     * @param text Text to be transformed
+     * @return Transformed text
+     */
     @Override
     public String transform(String text) {
         return transformer.transform(text);
--- src/main/java/pl/put/poznan/transformer/logic/UppercaseDecorator.java
+++ src/main/java/pl/put/poznan/transformer/logic/UppercaseDecorator.java
@@ -1,12 +1,25 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * Class for transforming text to uppercase.
+ * It extends BasicTextTransformer and implements TextTransformer.
+ */
 public class UppercaseDecorator extends BasicTextTransformer {
     private final TextTransformer transformer;
 
+    /**
+     * Constructor for UppercaseDecorator.
+     * @param wrappedTransformer TextTransformer object to be decorated.
+     */
     public UppercaseDecorator(TextTransformer wrappedTransformer) {
         transformer = wrappedTransformer;
     }
 
+    /**
+     * Method for transforming text.
+     * @param text Text to be transformed
+     * @return Text converted to uppercase
+     */
     @Override
     public String transform(String text) {
         return transformer.transform(text).toUpperCase();
